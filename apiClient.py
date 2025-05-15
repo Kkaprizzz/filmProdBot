33bc94d9f946b6e395fa392599ceacbd2eea8dd6
Kkaprizzz <sacha12765@gmail.com>
2025-05-15 22:33:43 +0300

    apiClient.py

--- apiClient.py
+++ apiClient.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify # type: ignore
 import database
 import handlers as hnd
 import asyncio
+import os
 
 app = Flask(__name__)
 
@@ -89,4 +90,5 @@ def edit_film_image():
     return jsonify({'message': 'Film image updated successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
